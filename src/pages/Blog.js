f7117c6e0bd8ea7954793ac6b1dee228a62c3163
Mikołaj <karwacki.mikolaj123@gmail.com>
2024-01-24 18:43:22 +0100

    update structure off blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,89 +1,39 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import BlogList from "../components/BlogList";
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      id: 1,
-      title: "Blog 1",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-    },
-    {
-      id: 2,
-      title: "Blog 2",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-    },
-  ]);
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [newBlog, setNewBlog] = useState({ title: "", description: "" });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewBlog((prevBlog) => ({ ...prevBlog, [name]: value }));
-  };
-
-  const handleAddBlog = () => {
-    const highestId = Math.max(...blogs.map((blog) => blog.id), 1);
-
-    setBlogs((prevBlogs) => [
-      ...prevBlogs,
-      {
-        id: highestId + 1,
-        title: newBlog.title,
-        description: newBlog.description,
-      },
-    ]);
-
-    setNewBlog({ title: "", description: "" });
-  };
-
-  const handleDeleteBlog = (idToDelete) => {
-    const updatedBlogs = blogs.filter((blog) => blog.id !== idToDelete);
-    setBlogs(updatedBlogs);
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("Could not fetch the data for that resource");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 500);
+  }, []);
 
   return (
-    <div className="blog-container">
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>{blog.description}</p>
-            <div>
-              <button onClick={() => handleDeleteBlog(blog.id)}>Delete</button>
-              <button>Read More</button>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h2>Add a New Blog</h2>
-      <form>
-        <label>
-          Title:
-          <input
-            type="text"
-            name="title"
-            value={newBlog.title}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Description:
-          <textarea
-            name="description"
-            value={newBlog.description}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handleAddBlog}>
-          Add Blog
-        </button>
-      </form>
+    <div className="blog">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
 
-export default Blog;
+export default Home;
