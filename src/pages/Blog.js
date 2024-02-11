c3a9e85cba8b8d5bfde0eace84d458131a1a7ac6
Mikołaj <karwacki.mikolaj123@gmail.com>
2024-02-11 17:52:37 +0100

    add RWD mobile and tablet

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -5,7 +5,7 @@ const Home = () => {
   const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="blog">
-      {error && <div>{error}</div>}
+      {error && <div className="error">{error}</div>}
       {isPending && <div className="load">Loading...</div>}
       {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
